7cd446aa13ae4e6d83388864b383968da808a648
youpeng <wuyoupeng2001@outlook.com>
2025-07-17 08:03:43 +0200

    update

--- satlib.py
+++ satlib.py
@@ -21,14 +21,15 @@ def calculate_hist_stats(data_list, sample_per_exp, bins=None):
     
     # 计算实验次数并截断数据
     n_exp = total_samples // sample_per_exp
-    if total_samples % sample_per_exp != 0:
-        print(f"Warning: 截断 {total_samples - n_exp*sample_per_exp} 个无法分组的数据")
-        data = data[:n_exp*sample_per_exp]
     if n_exp == 0:
-        # 使用np.randomchoice倍增数据达到实验次数
+        # 先处理数据不足的情况
         n_exp = 1
         data = np.random.choice(data, size=sample_per_exp, replace=True)
-        print(f"Warning: 实验次数不足，使用随机抽样数据达到实验次数 {n_exp}")
+        print(f"*", end=' ')
+    # 再处理数据截断的情况
+    if total_samples % sample_per_exp != 0:
+        print(f"#", end=' ')
+        data = data[:n_exp*sample_per_exp]
     # 分割为多组实验数据
     data_groups = data.reshape(n_exp, sample_per_exp)
     
